21d2e3331b6eca1b9eeb4084a2b02338c905d4b9
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-04-03 19:55:09 -0700

    Display region name under specific location

--- src/ui/header.js
+++ src/ui/header.js
@@ -1,19 +1,22 @@
 import dateFormat from "dateformat";
 
 export default function loadHeader(location, current, forecastToday) {
-    const locationName = document.querySelector('h3.location');
+    const locationName = document.querySelector('.location');
     locationName.textContent = location.name;
 
-    const temp = document.querySelector('h1.temp');
+    const regionName = document.querySelector('.region');
+    regionName.textContent = location.region;
+
+    const temp = document.querySelector('.temp');
     temp.textContent = `${localStorage.getItem('TEMP_UNIT') == 'F' ? Math.round(current.temp_f) : Math.round(current.temp_c)}°`;
 
-    const condition = document.querySelector('p.condition');
+    const condition = document.querySelector('.condition');
     condition.textContent = current.condition.text;
 
-    const high = document.querySelector('p.high');
+    const high = document.querySelector('.high');
     high.textContent = `H: ${localStorage.getItem('TEMP_UNIT') == 'F' ? forecastToday.maxtemp_f : forecastToday.maxtemp_c}°`;
 
-    const low = document.querySelector('p.low');
+    const low = document.querySelector('.low');
     low.textContent = `L: ${localStorage.getItem('TEMP_UNIT') == 'F' ? forecastToday.mintemp_f : forecastToday.mintemp_c}°`;
 
     const currentDate = new Date(location.localtime);
